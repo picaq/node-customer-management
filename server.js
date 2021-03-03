2f0789abe4ea848c31f2065de41f00d2de98826c
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-03 15:35:38 -0800

    create server to run on port 3000 as default

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const bodyParser = require('body-parser');
 
 // 2. Set port variable
 const port = process.env.PORT || 3000;
+
+// 3. Create the server, making sure to add the body-parser functions after the app has been created
+// const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: true } ));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}...`);
+});
