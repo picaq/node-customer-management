328a54422be2542d4d1a588bbd6ceb90a9e521c1
Mandy <34908590+picaq@users.noreply.github.com>
2021-03-03 15:24:47 -0800

    initialize REST API guided practice II

---
+++ server.js
@@ -0,0 +1,7 @@
+// 1. Import the express and body-parser packages
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// 2. Set port variable
+const port = process.env.PORT || 3000;
